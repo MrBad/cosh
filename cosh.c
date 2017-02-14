443786ee7bb65886d83d4d22af395091092b688f
MrBadNews <viorel.irimia@gmail.com>
2017-02-14 23:26:57 +0200

    clean

--- cosh.c
+++ cosh.c
@@ -8,23 +8,10 @@
 // buf to read in the line, max number of chars
 // return length of line //
 int read_line(char *buf, int max) {
-	int n, i;
+	int n;
 	n = read(0, buf, max-1);
 	if(n == 0) return n;
 	buf[n] = 0;
-	// clean left //
-	for(i = 0; i < n; i++) {
-		if(buf[i]!=' '&&buf[i]!='\t') {
-			n = n - i;
-			strncpy(buf, buf+i, n);
-			buf[n]=0;
-			break;
-		}
-	}
-	// clean right //
-	while((n > 0)&&(buf[n-1]==' '||buf[n-1]=='\t'||buf[n-1]=='\r'||buf[n-1]=='\n'))
-		buf[--n]=0;
-
 	return n;
 }
 
@@ -33,12 +20,14 @@ char *read_token(char *buf, char *prev_token, unsigned int len)
 {
 	char *p;
 	if(prev_token == NULL) { // first time here
-		prev_token = buf;
 		for(p = buf; p < buf+len; p++) {
-			if(*p==' ' || *p=='\t') {
+			if(*p==' ' || *p=='\t'||*p=='\r'||*p=='\n') {
 				*p = 0;
 			}
 		}
+		for(p = buf; *p==0 && p < buf+len; p++)
+				;// find first non 0 char at the begining; 
+		prev_token = p;
 		return prev_token;
 	}
 	int found = 0;
@@ -83,7 +72,7 @@ int main() {
 
 		pid = fork();
 		if(pid == 0) {
-			exit(execvp(argv[0], argv) < 0);
+			exit(execvp(argv[0], argv));
 		} else if(pid < 0) {
 			printf("error forking\n");
 		} else {
